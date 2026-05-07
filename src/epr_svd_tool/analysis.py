451d4c292401d96bcb1104c184f0492531e5d2c4
cl2798-debug <cl2798@cornell.edu>
2026-05-07 13:02:44 -0400

    Update analysis.py

--- src/epr_svd_tool/analysis.py
+++ src/epr_svd_tool/analysis.py
@@ -3,9 +3,6 @@ from .svd import svd_denoise_matrix
 
 
 def prepare_kinetic_matrix(field, matrix, poly_order=1, rank=3):
-    """
-    Full preprocessing + SVD workflow.
-    """
     matrix_bc = baseline_correct_columns(field, matrix, poly_order=poly_order)
     matrix_norm = normalize_columns(matrix_bc)
     matrix_svd, singular_values = svd_denoise_matrix(matrix_norm, rank=rank)
